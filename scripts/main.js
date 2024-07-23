e1381bbeb8aac11ec08efdade542078a80777709
Nathan Scott <dev@ngscott.net>
2024-07-24 01:30:21 +1000

    Add packaging functions

--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,10 @@ import { getExtension, isXML } from './utils.js';
 
 const upload = document.getElementById('upload');
 const output = document.getElementById('output');
+const downloadZip = document.getElementById('download-zip');
+const downloadXml = document.getElementById('download-xml');
 
+let fileName = 'document';
 const parts = new XMap();
 
 upload.addEventListener('change', async event => {
@@ -109,3 +112,119 @@ upload.addEventListener('change', async event => {
   }
 
 });
+
+async function createZipPackage (parts) {
+  const newParts = {};
+  const contentTypePaths = new XMap();
+  const defaultExtensions = new Map([
+    ['rels', 'application/vnd.openxmlformats-package.relationships+xml'],
+    ['xml', 'application/xml'],
+    ['txt', 'text/plain'],
+    ['htm', 'text/html'],
+    ['html', 'text/html'],
+    ['pdf', 'application/pdf'],
+    ['gif', 'image/gif'],
+    ['jpg', 'image/jpg'],
+    ['jpeg', 'image/jpg'],
+    ['png', 'image/png'],
+    ['tiff', 'image/tiff'],
+  ]);
+  const contentTypeExtensions = new XMap();
+  const encoder = new TextEncoder();
+
+  for (const path of [...parts.keys()].sort()) {
+    const { contentType, content, xml, enc } = parts.get(path);
+    const npath = path.replace(/^[/]?/, '');
+    const extn = getExtension(path);
+    contentTypePaths.add(path, contentType);
+    if (defaultExtensions.has(extn)) {
+      if (contentType === defaultExtensions.get(extn)) contentTypeExtensions.set(extn, defaultExtensions.get(extn));
+    } else {
+      if (!contentTypeExtensions.has(extn)) contentTypeExtensions.add(extn, contentType);
+      else contentTypeExtensions.set(extn, null);
+    }
+
+    if (content instanceof XMLDocument) {
+      const serialised = serialiseXML(content);
+      newParts[npath] = encoder.encode(serialised);
+    } else if (typeof content === 'string' || content instanceof String) {
+      newParts[npath] = encoder.encode(content);
+    } else newParts[npath] = content;
+
+    // newParts[path] = encoder.encode(output);
+    // newParts[npath] = new Uint8Array([0]);
+  }
+
+  // const compressed = zipSync({
+  //   'dir1': {
+  //     'nested': {
+  //       '你好.txt': encoder.encode('Hey there!')
+  //     },
+  //     'other/tmp.txt': new Uint8Array([97, 98, 99, 100])
+  //   },
+  //   'superTinyFile.bin': [new Uint8Array([0]), { level: 0 }]
+  // }, {
+  //   level: 1
+  // });
+  // const decompressed = unzipSync(compressed);
+  // console.log({compressed, decompressed});
+
+  const ctStream = newXML({ standalone: true });
+  ctStream.append(ctStream.createElementNS(NS_CONTENT_TYPES, 'Types'));
+  for (const [extension, type] of contentTypeExtensions) {
+    const node = ctStream.createElementNS(NS_CONTENT_TYPES, 'Default');
+    node.setAttribute('Extension', extension);
+    node.setAttribute('ContentType', type);
+    ctStream.documentElement.append(node);
+  }
+  for (const [path, type] of contentTypePaths) {
+    if (contentTypeExtensions.get(getExtension(path)) === type) continue;
+    const node = ctStream.createElementNS(NS_CONTENT_TYPES, 'Override');
+    node.setAttribute('PartName', path);
+    node.setAttribute('ContentType', type);
+    ctStream.documentElement.append(node);
+  }
+  console.log(serialiseXML(ctStream));
+  newParts[PATH_CONTENT_TYPES] = encoder.encode(serialiseXML(ctStream));
+
+  console.log({ ctStream, newParts });
+  const zipped = await zip(newParts, { level: 1 });
+  const zipfile = new File([zipped], `${fileName}.docx`, { type: MIME_DOCX });
+  console.log({ zipped, zipfile });
+  const a = document.createElement('a');
+  const href = URL.createObjectURL(zipfile);
+  a.href = href;
+  a.download = zipfile.name;
+  a.click();
+  const data = new Uint8Array(await zipfile.arrayBuffer());
+  console.log({ data });
+  const unzipped = await unzip(data);
+  console.log({ unzipped });
+}
+
+function createXmlPackage (parts) {
+  const base = parseXML([
+    '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>',
+    '<?mso-application progid="Word.Document"?>',
+    '<pkg:package xmlns:pkg="http://schemas.microsoft.com/office/2006/xmlPackage" />'
+  ].join(''));
+  for (const path of [...parts.keys()].sort()) {
+    const { contentType, content, xml, enc } = parts.get(path);
+    const part = base.createElementNS(NS_PKG, 'part');
+    if (xml) {
+      const xmlData = base.createElementNS(NS_PKG, 'xmlData');
+      const data = base.importNode(content.documentElement, true);
+      part.setAttributeNS(NS_PKG, 'name', path);
+      part.setAttributeNS(NS_PKG, 'contentType', contentType);
+      xmlData.append(data);
+      part.append(xmlData);
+    } else part.textContent = content;
+    base.documentElement.append(part);
+  }
+  console.log({ base });
+  const output = serialiseXML(base);
+  console.log({ output });
+}
+
+downloadXml.addEventListener('click', event => createXmlPackage(parts));
+downloadZip.addEventListener('click', async event => await createZipPackage(parts));
