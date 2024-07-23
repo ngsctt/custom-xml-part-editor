81fa6135f6399629b48dbad1d91783cb4a333169
Nathan Scott <dev@ngscott.net>
2024-07-24 01:28:29 +1000

    Improve parsing and extraction

--- scripts/main.js
+++ scripts/main.js
@@ -9,14 +9,17 @@ import { getExtension, isXML } from './utils.js';
 const upload = document.getElementById('upload');
 const output = document.getElementById('output');
 
+const parts = new XMap();
 
 upload.addEventListener('change', async event => {
   while (output.firstChild) output.lastChild.remove();
-  const {files} = event.target;
+  const { files } = event.target;
   const file = files[0];
-  const partPaths = new XMap();
+  fileName = file.name.replace(/(?<=.+)[.][^.]+$/, '');
+  console.log({ fileName });
   const contentTypePaths = new XMap();
   const contentTypeExtensions = new XMap();
+  parts.clear();
 
   async function loadContentTypes (uint8array) {
 
@@ -25,15 +28,15 @@ upload.addEventListener('change', async event => {
       while (node) {
         map.add(replacer(node.getAttribute(attr)), node.getAttribute('ContentType'));
         node = results.iterateNext();
-      }  
+      }
     }
-    
+
     const text = await new Blob([uint8array]).text();
     const xml = parseXML(text);
     const defaults = xml.evaluate('/_:Types/_:Default', xml.documentElement, () => NS_CONTENT_TYPES, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
     const overrides = xml.evaluate('/_:Types/_:Override', xml.documentElement, () => NS_CONTENT_TYPES, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
     resultsToEntries(defaults, 'Extension', x => x.toLowerCase(), contentTypeExtensions);
-    resultsToEntries(overrides, 'PartName', x => x.replace(/^\//, '/') , contentTypePaths);
+    resultsToEntries(overrides, 'PartName', x => x.replace(/^\//, '/'), contentTypePaths);
   }
 
   if (file.type === MIME_DOCX) {
@@ -43,103 +46,66 @@ upload.addEventListener('change', async event => {
       await loadContentTypes(unzipped[PATH_CONTENT_TYPES]);
       delete unzipped[PATH_CONTENT_TYPES];
     }
-    for (const [rawPath, content] of Object.entries(unzipped)) {
-      const path = rawPath.replace(/^\/?/, '/');
-      const extension = getExtension(path);
-      const data = { contentType, content };
-      if (contentTypePaths.has(path)) data.contentType = contentTypePaths.get(path);
-      else if (contentTypeExtensions.has(extension)) data.contentType = contentTypeExtensions.get(extension);
-      partPaths.set(path, data);
     // console.log(unzipped);
+    for (const [path, bytes] of Object.entries(unzipped)) {
+      const npath = path.replace(/^\/?/, '/');
+      const extension = getExtension(npath);
+      let contentType, content, enc, xml = false;
+      if (contentTypePaths.has(npath)) contentType = contentTypePaths.get(npath);
+      else if (contentTypeExtensions.has(extension)) contentType = contentTypeExtensions.get(extension);
+      if (isXML(contentType)) {
+        xml = true;
+        enc = detect(bytes);
+        const text = convert(bytes, { to: 'unicode', from: enc, type: 'string' });
+        content = parseXML(text);
+      } else content = bytes;
+      parts.set(npath, { contentType, content, xml, enc });
     }
   } else if (MIME_XML.includes(file.type)) {
-    // const xml = parse(await file.text());
-    // const parser = new DOMParser();
-    // const xml = parser.parseFromString(await file.text(), "application/xml");
-    const xml = _xml.parse(await file.text());
-    // console.log(xml);
-    const pretty = _xml.pretty(xml, true);
-    // console.log({pretty});
-    // const parts = xml.querySelectorAll('ns|a');
-    // console.log({parts});
+    const xml = parseXML(await file.text());
+    const enc = 'UTF-8';
+    const pkgParts = xml.getElementsByTagNameNS(NS_PKG, 'part');
 
-    // const nodesSnapshot = xml.evaluate(
-    //   "//pkg:part",
-    //   xml,
-    //   null,
-    //   XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
-    //   null,
-    // );
-    
-    // for (let i = 0; i < nodesSnapshot.snapshotLength; i++) {
-    //   console.log(nodesSnapshot.snapshotItem(i).textContent);
-    // }
-
-    const parts = xml.getElementsByTagNameNS(NS_PKG, 'part');
-    console.log({parts});
-    for (const part of parts) {
+    for (const part of pkgParts) {
       const name = part.getAttributeNS(NS_PKG, 'name');
       const contentType = part.getAttributeNS(NS_PKG, 'contentType');
-      console.log({name, contentType, part});
-      const doc = document.implementation.createDocument('', '', null);
-      for (const child of part.firstElementChild.children) {
-        doc.append(doc.importNode(child, true));
-      }
-      console.log({doc});
-      partPaths.set(name, doc);
+      // console.log({ name, contentType, part });
+      let content, xml = false;
+      if (isXML(contentType)) {
+        xml = true;
+        content = document.implementation.createDocument('', '', null);
+        const declaration = content.createProcessingInstruction('xml', `version="1.0" encoding="${enc}" standalone="yes"`);
+        content.append(declaration);
+        for (const child of part.firstElementChild.children) content.append(content.importNode(child, true));
+      } else content = part.textContent;
+      parts.set(name, { contentType, content, xml, enc });
+      console.log(serialiseXML(content));
     }
   }
 
-  const root = {};
-  console.log({paths: [...partPaths.keys()] });
-  for (const p of partPaths.keys()) {
-    const levels = p.replace(/^\/?/, '').split('/');
-    let c = root;
-    while (levels.length > 1) {
-      const l = levels.shift();
-      if (!c[l]) c[l] = new Dir(l);
-      c = c[l].children;
-    }
-    c[levels[0]] = partPaths.get(p);
+  for (const key of [...parts.keys()].sort()) {
+    const { contentType, content } = parts.get(key);
+    const details = document.createElement('details');
+    details.classList.add('pkg-part');
+    const summary = document.createElement('summary');
+    const metadata = document.createElement('p');
+    metadata.classList.add('pkg-metadata');
+    summary.textContent = key;
+    metadata.innerHTML = `content type: "${contentType}"`;
+    details.append(summary);
+    details.append(metadata);
+    if (content instanceof XMLDocument) {
+      const pre = document.createElement('pre');
+      pre.classList.add('pkg-xml');
+      const text = serialiseXML(content);
+      pre.append(prettifyXML(text));
+      details.append(pre);
+    } else if (isImg(contentType)) {
+      const img = new Image()
+      img.src = URL.createObjectURL(new Blob([content], { type: contentType }));
+      details.append(img);
+    } else details.append(content.toString());
+    output.append(details);
   }
-  console.log({root});
-
-  // output.innerHTML = paths.join('<br>');
-
-  async function makeDetails (dir) {
-    const items = [];
-    for (const key of Object.keys(dir).sort()) {
-      const value = dir[key];
-      const details = document.createElement('details');
-      const summary = document.createElement('summary');
-      summary.textContent = key;
-      details.append(summary);
-      let text;
-      if (value instanceof Dir) {
-        details.append(...await makeDetails(value.children))
-        details.open = true;
-      } else {
-        let text;
-        if (value instanceof Uint8Array) {
-          const enc = detect(value);
-          text = convert(value, {to: 'unicode', from: enc, type: 'string'});
-        } else if (value instanceof XMLDocument) {
-          text = new XMLSerializer().serializeToString(value);
-        }
-        // console.log({value});
-        // const blob = new Blob([value]);
-        // console.log({blob});
-        // const text = await blob.text();
-        // console.log(text);
-        const pre = document.createElement('pre');
-        pre.append(prettifyXML(text));
-        details.append(pre);
-        // console.log(text, prettifyXML(text));
-      }
-      items.push(details);
-    }
-    return items;
-  }
-  output.append(...await makeDetails(root));
 
 });
